ea4fcb805a4d577b2070b61083bd22e57ec1389b
Martin Bakke <martinba88@gmail.com>
2014-08-29 12:23:02 +0200

    Test av Azure connection strings

--- index.php
+++ index.php
@@ -1,3 +1,30 @@
 <?php
 
-phpinfo();
+// phpinfo();
+
+function connStrToArray($conn_str){
+ 
+    // Initialize array.
+    $conn_array = array();
+ 
+    // Split conn string on semicolons. Results in array of "parts".
+    $parts = explode(";", $conn_str);
+ 
+ 
+    // Loop through array of parts. (Each part is a string.)
+    foreach($parts as $part){
+ 
+        // Separate each string on equals sign. Results in array of 2 items.
+        $temp = explode("=", $part);
+ 
+        // Make items key=>value pairs in returned array.
+        $conn_array[$temp[0]] = $temp[1];
+    }
+ 
+    return $conn_array;
+}
+
+$connstring = getenv("DefaultConnection");
+$connarray = connStrToArray($connstring);
+
+print_r($connarray);
